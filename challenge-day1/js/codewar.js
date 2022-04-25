b6cf958bc96f64c79fb278e76559aa26371a9a1c
bee-thedev <beenyshsaeed@gmail.com>
2022-04-25 23:07:40 +0500

    Day9-Challenge#3 added

--- challenge-day1/js/codewar.js
+++ challenge-day1/js/codewar.js
@@ -449,3 +449,30 @@ function removeChar(str){
 
  
  console.log(removeChar("eloquent"))
+
+ // Day9 - Challenge#2: Write function bmi that calculates body mass index (bmi = weight / height2).
+
+//if bmi <= 18.5 return "Underweight"
+
+//if bmi <= 25.0 return "Normal"
+
+//if bmi <= 30.0 return "Overweight"
+
+//if bmi > 30 return "Obese"
+
+
+function bmi(weight, height) {
+  let bmi = weight / (height * height);
+  if(bmi <= 18.5){
+    return "Underweight"
+  }else if(bmi <= 25.0){
+    return "Normal"
+  }else if(bmi <= 30.0){
+    return "Overweight"
+  }else if ( bmi >= 30){
+    return "Obese"
+  }
+  return bmi;
+}
+
+console.log(bmi(60, 1.54))
