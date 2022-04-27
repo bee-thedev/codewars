ad97028dcefafa5bb0c20314f46e371f06054fc8
bee-thedev <beenyshsaeed@gmail.com>
2022-04-27 23:23:38 +0500

    Day11-Challenges#5 n #6 added

--- codewars2/codewars2.js
+++ codewars2/codewars2.js
@@ -46,3 +46,55 @@ function booleanToString(b){
   }
 
   console.log(booleanToString(true))
+
+
+  //Day11-Challenge#5 : Create a function with two arguments that will 
+  //return an array of the first (n) multiples of (x).
+
+//Assume both the given number and the number of times to count will be positive numbers greater than 0.
+
+//countBy(1,10) === [1,2,3,4,5,6,7,8,9,10]
+//countBy(2,5) === [2,4,6,8,10]
+
+function countBy(x, n) {
+  let z = [];
+  
+  for (let i = 1; i <= n ; i++){
+    let multiple = (x*i)
+    z.push(multiple)
+  }
+  return z;
+}
+
+console.log(countBy(11,10))
+
+//Day11 -Challenge#6: Task:
+//Your task is to write a function which returns the sum of following series upto nth term(parameter).
+
+//Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+
+//Rules:
+//You need to round the answer to 2 decimal places and return it as String.
+
+//If the given value is 0 then it should return 0.00
+
+//You will only be given Natural Numbers as arguments.
+
+//Examples:(Input --> Output)
+//1 --> 1 --> "1.00"
+//2 --> 1 + 1/4 --> "1.25"
+//5 --> 1 + 1/4 + 1/7 + 1/10 + 1/13 --> "1.57"
+
+function SeriesSum(n)
+{
+  let sum = 0;
+  for(let i = 0; i < n; i++){
+    sum = sum + (1/(1+i*3))
+  }
+  return sum.toFixed(2).toString();
+}
+
+console.log(SeriesSum(5))
+
+
+
