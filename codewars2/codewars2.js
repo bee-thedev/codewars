ecf6773c9fe8074e454fc8612e4135784e99d1dd
bee-thedev <beenyshsaeed@gmail.com>
2022-04-28 20:43:53 +0500

    Day12-Challenge#2 added

--- codewars2/codewars2.js
+++ codewars2/codewars2.js
@@ -115,3 +115,20 @@ function positiveSum(arr) {
   })
   return sum;
 }
+
+
+//Day12-Challenege#2: 
+//If you can't sleep, just count sheep!!
+//Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+
+var countSheep = function (num){
+  let sheepcount = "";
+  for( let i = 1; i<=num; i++){
+      sheepcount = sheepcount + `${i} sheep...` ;
+  }
+  return sheepcount
+}
+
+console.log(countSheep(6))
+
+
