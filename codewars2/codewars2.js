84fba43707f6e77f486cb50e9cd79c33510674e1
Beenysh Sara <beenyshsaeed@gmail.com>
2022-04-28 13:59:38 +0500

    Day12-Challenge#1 added

--- codewars2/codewars2.js
+++ codewars2/codewars2.js
@@ -98,3 +98,20 @@ console.log(SeriesSum(5))
 
 
 
+//Day12-Challenge#1:You get an array of numbers, return the sum of all of the positives ones.
+
+//Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+//Note: if there is nothing to sum, the sum is default to 0.
+
+function positiveSum(arr) {
+  let sum = 0;
+  arr.map(number =>{
+    if(number >=0){
+      sum = sum + number
+    }else{
+      sum = sum
+    }
+  })
+  return sum;
+}
