5920633dd8eed02c04f48524ecce9d8397b99ca9
bee-thedev <beenyshsaeed@gmail.com>
2022-04-29 15:35:48 +0500

    Day13-Challenge#1 added

--- codewars2/codewars2.js
+++ codewars2/codewars2.js
@@ -152,3 +152,21 @@ function getGrade (s1, s2, s3) {
 }
 
 console.log(getGrade(67,89,100))
+
+
+//Day13-- Challenge#1: Is this a triangle?
+//Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
+
+//(In this case, all triangles must have surface greater than 0 to be accepted).
+
+
+function isTriangle(a,b,c)
+{
+  if(a+b>c && b+c>a && c+a>b){
+    return true;
+  }else{
+    return false
+  }
+}
+
+console.log(isTriangle(3,6,9))
