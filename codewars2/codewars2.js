41d42180ebbd5f298525ff01682d80c3605df7fa
bee-thedev <beenyshsaeed@gmail.com>
2022-04-29 20:54:45 +0500

    Day13-Challenge#3 added

--- codewars2/codewars2.js
+++ codewars2/codewars2.js
@@ -195,3 +195,37 @@ function stray(numbers) {
 }
 
 console.log(stray([50,-1,50,50,50,50]))
+
+
+//Day13-Challenge: 
+
+//Given a list of integers, determine whether the sum of its elements is odd or even.
+
+//Give your answer as a string matching "odd" or "even".
+
+//If the input array is empty consider it as: [0] (array with a zero).
+
+//Examples:
+//Input: [0]
+//Output: "even"
+
+//Input: [0, 1, 4]
+//Output: "odd"
+
+//Input: [0, -1, -5]
+//Output: "even"
+
+
+function oddOrEven(array) {
+  let accumulatedSum = 0;
+   array.forEach(number=> {
+       return  accumulatedSum += number
+      })
+      if(accumulatedSum % 2 === 0){
+        return "even"
+      }else if(accumulatedSum %2 !== 0){
+        return "odd"
+      }
+}
+
+console.log(oddOrEven([3,3,8,0,4,5,-7]))
