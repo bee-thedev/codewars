f786495ebea5eb0366277cd3debd2d0d95803700
beehance <beenyshsara@outlook.com>
2022-04-30 16:11:39 +0500

    Day14-challenge#1 added

--- codewars2/codewars2.js
+++ codewars2/codewars2.js
@@ -229,3 +229,31 @@ function oddOrEven(array) {
 }
 
 console.log(oddOrEven([3,3,8,0,4,5,-7]))
+
+
+//Day14-Challenge#1: Return the number (count) of vowels in the given string.
+
+//We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+//The input string will only consist of lower case letters and/or spaces.
+
+// function getCount(str) {
+//   let vowelsCount = 0;
+  
+//   str.split("").filter(letter => letter=== "a" || letter === "e" || letter === "i" || letter === "o" || letter === "u" ? vowelsCount++ : " ")
+
+//   console.log(vowelsCount)
+//   return vowelsCount;
+// }
+
+function getCount(str) {
+  let vowelsCount = 0;
+  let strlower = str.toLowerCase()
+  strlower.split("").filter(letter => letter=== "a" || letter === "e" || letter === "i" || letter === "o" || letter === "u" ? vowelsCount++ : " ")
+
+  console.log(vowelsCount)
+  return vowelsCount;
+}
+
+getCount("MEESHAAL")
+
