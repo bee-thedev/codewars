e2d0f104e15c4d26db3fcc681d753654b0260fa2
bee-thedev <beenyshsaeed@gmail.com>
2022-05-01 23:49:48 +0500

    Day15-Challenge#1

--- codewars2/codewars2.js
+++ codewars2/codewars2.js
@@ -257,3 +257,30 @@ function getCount(str) {
 
 getCount("MEESHAAL")
 
+//Day15-Challenge#1: 
+
+//We want to know the index of the vowels in a given word, for example, there are two vowels in the word super (the second and fourth letters).
+
+//So given a string "super", we should return a list of [2, 4].
+
+// Some examples:
+//Mmmm  => []
+//Super => [2,4]
+//Apple => [1,5]
+//YoMama -> [1,2,4,6]
+
+//Vowels in this context refers to: a e i o u y (including upper case)
+//This is indexed from [1..n] (not zero indexed!)
+
+
+function vowelIndices(word){
+  let find = word.split(" ").filter(vowels => {
+    if(vowels === 'a' || vowels === 'i' || vowels === 'e' || vowels === 'o' || vowels === 'u' || vowels === 'y'){
+       word.indexOf(vowels)
+    }
+})
+return find
+}
+
+
+console.log(vowelIndices("beenysh"))
