0e4b44d3c720ada959270aca26d82d47d0d4c8d9
bee-thedev <beenyshsaeed@gmail.com>
2022-05-06 00:35:43 +0500

    Day16-Challenge1 updated

--- codewars3/index.js
+++ codewars3/index.js
@@ -5,3 +5,13 @@
 
 //Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
 //Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
+
+String.prototype.toJadenCase = function () {
+    let splitstring = this.split(" ");
+    for(i=0; i<splitstring.length; i++){
+        splitstring[i] = splitstring[i].CharAt(0).toUpperCase() + splitstring[i].substring(1);
+    }
+    return splitstring.join(' ');
+  };
+
+  console.log(str.toJadenCase())
