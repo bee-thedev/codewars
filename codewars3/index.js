863f2daba26aa6eae852a2fcfe6fbb8b3a762827
bee-thedev <beenyshsaeed@gmail.com>
2022-05-10 15:41:46 +0500

    Day16-Addon challenge

--- codewars3/index.js
+++ codewars3/index.js
@@ -62,6 +62,42 @@ return answer.length - newanswer.length;
 console.log(dontGiveMeFive(1,19))
 
 
+//Day16: Challenge #1:
+
+// write a function to calculate what day is today, like this:
+
+// Use case : switch.
+
+function howManydays(month){
+  var days;
+  switch (month){
+    case 2: 
+     days = 28
+    break;
+    
+    case 1: 
+    case 3: 
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12: 
+      days = 31;
+    break;
+    
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+       days = 30
+  }
+  return days
+}
+
+console.log(howManydays(4))
+
+
+
 
 
 
