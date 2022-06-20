b0222c679335b8dcde3772d88b6b7f689a691d83
bee-thedev <beenyshsaeed@gmail.com>
2022-06-20 21:49:58 +0500

    7kyu-Are you a square

--- 5thJune-Kyu/5thJune-Kyu.js
+++ 5thJune-Kyu/5thJune-Kyu.js
@@ -165,38 +165,6 @@ return newarray.filter(number=> number.length ===n)
 
 eachCons([1,2,3,4], 3)
 
-<<<<<<< HEAD
-
-//7kyu --You're a square!
-
-// DESCRIPTION:
-// A square of squares
-// You like building blocks. You especially like building blocks that are squares. And what you even like more, is to arrange them into a square of square building blocks!
-
-// However, sometimes, you can't arrange them into a square. Instead, you end up with an ordinary rectangle! Those blasted things! If you just had a way to know, whether you're currently working in vain… Wait! That's it! You just have to check if your number of building blocks is a perfect square.
-
-// Task
-// Given an integral number, determine if it's a square number:
-
-// In mathematics, a square number or perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself.
-
-// The tests will always use some integral number, so don't worry about that in dynamic typed languages.
-
-// Examples
-// -1  =>  false
-//  0  =>  true
-//  3  =>  false
-//  4  =>  true
-// 25  =>  true
-// 26  =>  false
-
-
-var isSquare = function(n){
-  return false; // fix me
-}
-
-isSquare()
-=======
 //7kyu-
 
 function GetSum( a, b )
@@ -216,4 +184,37 @@ function loop( i, j ){
 
 
 GetSum(4,8)
->>>>>>> fb3209611df8fb7f993b3748b0f6adf03f08c39f
+
+
+//7KYU- You're a square!
+
+// A square of squares
+// You like building blocks. You especially like building blocks that are squares. 
+//And what you even like more, is to arrange them into a square of square building blocks!
+
+// However, sometimes, you can't arrange them into a square. 
+//Instead, you end up with an ordinary rectangle! Those blasted things! 
+//If you just had a way to know, whether you're currently working in vain… Wait! 
+//That's it! You just have to check if your number of building blocks is a perfect square.
+
+// Task
+// Given an integral number, determine if it's a square number:
+
+// In mathematics, a square number or perfect square is an integer that is the square of an integer;
+// in other words, it is the product of some integer with itself.
+
+// The tests will always use some integral number, so don't worry about that in dynamic typed languages.
+
+// Examples
+// -1  =>  false
+//  0  =>  true
+//  3  =>  false
+//  4  =>  true
+// 25  =>  true
+// 26  =>  false
+
+var isSquare = function(n){
+  return false; // fix me
+}
+
+isSquare()
