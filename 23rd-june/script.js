6157f5931dd1dcd8f278bd33a7bc00d84c73b47d
bee-thedev <beenyshsaeed@gmail.com>
2022-07-10 23:42:17 +0500

    7kyu-Isograms

--- 23rd-june/script.js
+++ 23rd-june/script.js
@@ -306,3 +306,31 @@ function squareDigits(num){
 }
 
 squareDigits(9119);
+
+//7kyu-Isogram
+
+// An isogram is a word that has no repeating letters, consecutive or non-consecutive.
+// Implement a function that determines whether a string that contains only letters is an isogram. 
+//Assume the empty string is an isogram. Ignore letter case.
+
+// Example: (Input --> Output)
+
+// "Dermatoglyphics" --> true
+// "aba" --> false
+// "moOse" --> false (ignore letter case)
+
+function isIsogram(str){
+  let splitString = str.split("")
+  console.log(splitString)
+  for(let i =0; i<str.length; i++){
+  if(str[i] !== str[i+1]){
+    return true;
+  }else{
+    return false;
+  }
+}
+}
+
+
+
+isIsogram("Dermatoglyphics");
