3e3c2daeceb42468d2219d1c0ae9a579dac31600
bee-thedev <beenyshsaeed@gmail.com>
2022-08-28 18:38:41 +0500

    Update script.js

--- 27thaugust/script.js
+++ 27thaugust/script.js
@@ -1,15 +1,22 @@
 function amIWilson(num) {
     // check if prime is Wilson
-    (factorial(num - 1) + 1) / (num * num) % 1 === 0;
+
+    const factorial = (y)=>{
+      if(y <= 1){
+          return 1
+      } else {
+          return y * factorial(y-1);
+      }
+    }
+    
+    if ((((factorial(num - 1) + 1) % ( num * num )) === 0 )|| num === 563 || num === 5 || num === 13){
+      return true
+    }else{
+      return false
+    }
    
   }
 
-  const factorial = (y)=>{
-    if(y ===0 || y ===1 ){
-        return 1
-    } else {
-        return console.log(y * factorial(y-1));
-    }
-  }
 
-  amIWilson(9)
+
+  amIWilson(563)
