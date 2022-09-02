b7080b0946cd715134e6cd8979a39f539766b70c
beehance <beenyshsara@outlook.com>
2022-09-02 13:13:58 -0500

    2nd September onwards added

--- 19thJuly/script.js
+++ 19thJuly/script.js
@@ -49,7 +49,17 @@ function removeEveryOther(arr){
 // (80, "celery")
 
 function countVegetables(string){
+  let veggies = []
+  let sorted;
   
+  for(let i=0; i<string.length; i++){
+    sorted= string.split(',')
+  }
+  console.log(sorted)
 }
 
-countVegetables("cabbage", "carrot", "celery", "cucumber", "mushroom", "onion", "pepper", "potato", "tofu", "turnip");
+countVegetables("mushroom chopsticks chopsticks turnip mushroom carrot mushroom cabbage mushroom carrot tofu pepper cabbage potato cucumber  mushroom mushroom mushroom potato turnip chopsticks cabbage celery celery turnip pepper chopsticks potato potato onion cabbage cucumber
+onion pepper onion cabbage potato tofu carrot cabbage cabbage turnip mushroom cabbage cabbage cucumber cabbage chopsticks turnip pepper
+onion pepper onion mushroom turnip carrot carrot tofu onion tofu chopsticks chopsticks chopsticks mushroom cucumber chopsticks carrot
+potato cabbage cabbage carrot mushroom chopsticks mushroom celery turnip onion carrot turnip cucumber carrot potato mushroom turnip 
+mushroom cabbage tofu turnip turnip turnip mushroom tofu potato pepper turnip potato turnip celery carrot turnip");
