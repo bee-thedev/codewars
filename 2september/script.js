e213e72001fbf365d3fb2576bda9b6ff906fb5a2
Beenysh Sara <beenyshsaeed@gmail.com>
2022-09-04 23:37:00 +0500

    Update script.js

--- 2september/script.js
+++ 2september/script.js
@@ -24,3 +24,17 @@ function breakChocolate(n,m) {
  // because it uses the letters A-Z at least once (case is irrelevant).
 
 //Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
+
+
+//7kyu- Testing 1 2 3
+
+//Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+
+Write a function which takes a list of strings and returns each line prepended by the correct number.
+
+The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+Examples: (Input --> Output)
+
+[] --> []
+["a", "b", "c"] --> ["1: a", "2: b", "3: "c"]
