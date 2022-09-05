d522955cc4f812fc3fc14b25d92048efa184a272
Beenysh Sara <beenyshsaeed@gmail.com>
2022-09-05 23:44:17 +0500

    Update script.js

--- 2september/script.js
+++ 2september/script.js
@@ -38,3 +38,12 @@ Examples: (Input --> Output)
 
 [] --> []
 ["a", "b", "c"] --> ["1: a", "2: b", "3: "c"]
+
+
+//7 kyu
+Friend or Foe?
+Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"] 
